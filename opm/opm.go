9eab06ec708c8042a4e2ef9674883563e64b86b6
Alexander Czeczka <alexander.czeczka@scholzsue.de>
2016-09-19 02:28:44 +0200

    OpenPokeMap types/structs

---
+++ opm/opm.go
@@ -0,0 +1,52 @@
+package opm
+
+const (
+	POKEMON  = 1
+	POKESTOP = 2
+	GYM      = 3
+)
+
+type Account struct {
+	Username string
+	Password string
+	Provider string
+	Used     bool
+	Banned   bool
+}
+
+type Proxy struct {
+	Id string
+}
+
+type MapObject struct {
+	Type      int
+	PokemonId int
+	Id        string
+	Lat       float64
+	Lng       float64
+	Expiry    int64
+	Lured     bool
+	Team      int
+}
+
+type Pokemon struct {
+	EncounterId   string
+	PokemonId     int
+	Lat           float64
+	Lng           float64
+	DisappearTime int64
+}
+
+type Pokestop struct {
+	Id    string
+	Lat   float64
+	Lng   float64
+	Lured bool
+}
+
+type Gym struct {
+	Id   string
+	Lat  float64
+	Lng  float64
+	Team int
+}
